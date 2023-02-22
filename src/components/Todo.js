1c485cf2e5d3b55ad907516bf72406acde4031db
afterrest <afterrest00@gmail.com>
2023-02-22 23:37:52 +0900

    add task 기능추가

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,25 @@
 import React from "react";
 
-export default function Todo() {
+function Todo(props) {
     return (
-        // …
+        <li className="todo stack-small">
+            <div className="c-cb">
+                <input id={props.id} type="checkbox" defaultChecked={props.completed} />
+                <label className="todo-label" htmlFor={props.id}>
+                    {props.name}
+                </label>
+            </div>
+
+            <div className="btn-group">
+                <button type="button" className="btn">
+                    Edit <span className="visually-hidden">{props.name}</span>
+                </button>
+                <button type="button" className="btn btn__danger">
+                    Delete <span className="visually-hidden">{props.name}</span>
+                </button>
+            </div>
+        </li>
     );
 }
+
+export default Todo;
